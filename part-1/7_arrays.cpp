72d2309f3cce3fe2b332c9ec08541b42ac8271e4
Ashik044 <rezaashik044@gmail.com>
2024-01-06 11:41:24 +0600

    done

--- part-1/7_arrays.cpp
+++ part-1/7_arrays.cpp
@@ -27,15 +27,17 @@
 
 // int main() {
 //     // Declaration and Initialization
-//     int matrix[3][3] = {{1, 2, 3},
-//                         {4, 5, 6},
-//                         {7, 8, 9}};
+//     int twoDimensionalArray[3][4] = {
+//         {1, 2, 3, 4},
+//         {5, 6, 7, 8},
+//         {9, 10, 11, 12}
+//     };
 
-//     // Accessing elements
-//     cout << "Elements of the 2D array: " << endl;
-//     for (int i = 0; i < 3; ++i) {
-//         for (int j = 0; j < 3; ++j) {
-//             cout << matrix[i][j] << " ";
+//     // Accessing elements in a 2D array
+//     cout << "2D Array Elements:" << endl;
+//     for (int row = 0; row < 3; ++row) {
+//         for (int col = 0; col < 4; ++col) {
+//             cout << twoDimensionalArray[row][col] << " ";
 //         }
 //         cout << endl;
 //     }
@@ -47,31 +49,74 @@
 
 // * 3. Multi-dimensional Array:
 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Multi-dimensional Array
+    const int numLayers = 2;
+    const int numRows = 3;
+    const int numCols = 4;
+    
+    int multiDimensionalArray[numLayers][numRows][numCols] = {
+        {{1, 2, 3, 4}, 
+        {5, 6, 7, 8}, 
+        {9, 10, 11, 12}},
+        
+        {{13, 14, 15, 16}, 
+        {17, 18, 19, 20}, 
+        {21, 22, 23, 24}}
+    };
+
+    // Accessing elements in a 3D array
+    cout << "Elements of the 3D array:" << endl;
+    for (int layer = 0; layer < numLayers; ++layer) {
+        for (int row = 0; row < numRows; ++row) {
+            for (int col = 0; col < numCols; ++col) {
+                cout << multiDimensionalArray[layer][row][col] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+// * mini project using multi dimensional arrays
+
+// * Code:
+
 // #include <iostream>
 
 // using namespace std;
 
 // int main() {
-//     // Declaration and Initialization
-//     int cube[2][3][4] = {
-//         {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}},
-//         {{13, 14, 15, 16}, {17, 18, 19, 20}, {21, 22, 23, 24}}
+//     const int numStudents = 3;
+//     const int numCourses = 4;
+
+//     // Multi-dimensional array to store student grades
+//     int grades[numStudents][numCourses] = {
+//         {90, 85, 88, 92},
+//         {78, 89, 92, 87},
+//         {95, 94, 97, 91}
 //     };
 
-//     // Accessing elements
-//     cout << "Elements of the 3D array: " << endl;
-//     for (int i = 0; i < 2; ++i) {
-//         for (int j = 0; j < 3; ++j) {
-//             for (int k = 0; k < 4; ++k) {
-//                 cout << cube[i][j][k] << " ";
-//             }
-//             cout << endl;
+//     // Accessing and displaying grades
+//     cout << "Student Grades:" << endl;
+//     for (int student = 0; student < numStudents; ++student) {
+//         cout << "Student " << (student + 1) << ": ";
+//         for (int course = 0; course < numCourses; ++course) {
+//             cout << grades[student][course] << " ";
 //         }
 //         cout << endl;
 //     }
 
 //     return 0;
 // }
+
 
 // ----------------------------------------------------------------
 
@@ -135,37 +180,37 @@
 
 // * Here's an example of a vector of vectors representing a 2D array:
 
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    // Creating a vector of vectors (2D array)
-    vector<vector<int>> matrix;
+// int main() {
+//     // Creating a vector of vectors (2D array)
+//     vector<vector<int>> matrix;
 
-    // Initializing a 3x3 matrix
-    int rows = 3, cols = 3;
-    matrix.resize(rows, vector<int>(cols, 0));
+//     // Initializing a 3x3 matrix
+//     int rows = 3, cols = 3;
+//     matrix.resize(rows, vector<int>(cols, 0));
 
-    // Assigning values to the matrix
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            matrix[i][j] = i * cols + j + 1;
-        }
-    }
+//     // Assigning values to the matrix
+//     for (int i = 0; i < rows; ++i) {
+//         for (int j = 0; j < cols; ++j) {
+//             matrix[i][j] = i * cols + j + 1;
+//         }
+//     }
 
-    // Accessing elements of the matrix
-    cout << "Elements of the 2D array (vector of vectors):" << endl;
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+//     // Accessing elements of the matrix
+//     cout << "Elements of the 2D array (vector of vectors):" << endl;
+//     for (int i = 0; i < rows; ++i) {
+//         for (int j = 0; j < cols; ++j) {
+//             cout << matrix[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
 // In this example, vector<vector<int>> matrix is a vector of vectors representing a 2D array. Can extend this concept to create vectors with more dimensions as needed.
 
